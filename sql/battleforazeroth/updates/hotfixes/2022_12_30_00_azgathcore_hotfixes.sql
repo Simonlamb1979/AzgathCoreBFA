8726abc9df962bc57924e78e274c06bf1c48074a
Aurøra/Nehyren <dreamcorerev@gmail.com>
2022-12-30 06:11:48 +0100

    SQL/Hotfixes: Fixed the Cloak of Coordination for alliance side

---
+++ sql/battleforazeroth/updates/hotfixes/2022_12_30_00_azgathcore_hotfixes.sql
@@ -0,0 +1,7 @@
+-- hotfix_data
+
+INSERT IGNORE INTO `hotfix_data`(`Id`, `TableHash`, `RecordId`, `Deleted`, `VerifiedBuild`) VALUES (56645, 1073915313, 12926, 0, 0);
+
+-- item_effect
+
+INSERT IGNORE INTO `item_effect`(`ID`, `LegacySlotIndex`, `TriggerType`, `Charges`, `CoolDownMSec`, `CategoryCoolDownMSec`, `SpellCategoryID`, `SpellID`, `ChrSpecializationID`, `ParentItemID`, `VerifiedBuild`) VALUES (12926, 0, 0, 0, 7200000, -1, 0, 3561, 0, 65360, 0);
